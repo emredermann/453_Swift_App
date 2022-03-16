495f885fde89f87e39f44ea964a4a1ef96f3fabf
Emre Derman <37815224+emredermann@users.noreply.github.com>
2022-03-16 23:45:13 +0300

    Add files via upload

---
+++ 453_form/453_form/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  453_form
+//
+//  Created by Emre Derman on 3/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 453_form/453_formUITests/_53_formUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  _53_formUITestsLaunchTests.swift
+//  453_formUITests
+//
+//  Created by Emre Derman on 3/16/22.
+//
+
+import XCTest
+
+class _53_formUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
