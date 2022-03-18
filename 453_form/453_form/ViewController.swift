70cba8012bf2fb6a4932adb42ba958b7f5476dc8
Emre derman <emre.derman@ug.bilkent.edu.tr>
2022-03-18 11:02:19 +0300

    InputField changes.

--- 453_form/453_form/ViewController.swift
+++ 453_form/453_form/ViewController.swift
@@ -9,27 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
     @IBOutlet weak var name_surname_attr : UITextField!
-    @IBOutlet weak var birth_date_attr : UITextField!
-    @IBOutlet weak var city_attr : UITextField!
-    @IBOutlet weak var gender_attr : UITextField!
+    @IBOutlet weak var birth_date_attr : UIDatePicker!
     @IBOutlet weak var applied_vaccine_attr : UITextField!
     @IBOutlet weak var side_effect_vaccination_attr : UITextField!
     @IBOutlet weak var pcr_positive_cases_and_symproms_attr : UITextField!
     @IBOutlet weak var add_button : UIButton!
+    @IBOutlet weak var gender_picker: UIPickerView!
+    @IBOutlet weak var city_picker: UIPickerView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        add_button.isEnabled = button_visibility();
+        add_button.isHidden = button_visibility();
     }
-
+    
     func button_visibility() -> Bool{
+        print(name_surname_attr.text);
+        print(birth_date_attr.date.formatted());
+        print(applied_vaccine_attr.text);
+        print(side_effect_vaccination_attr.text);
+        print(pcr_positive_cases_and_symproms_attr.text);
+        
+        
+        
         if(name_surname_attr.hasText
-           && birth_date_attr.hasText
-           && city_attr.hasText
-           && gender_attr.hasText
+           && birth_date_attr.isEnabled
            && applied_vaccine_attr.hasText
            && side_effect_vaccination_attr.hasText
            && pcr_positive_cases_and_symproms_attr.hasText
@@ -39,19 +45,21 @@ class ViewController: UIViewController {
             return false;
         }
     }
+    
+    
     @IBAction func name_surname(_ sender: UITextField) {
         name_surname_attr.text = sender.text!;
     }
     
-    @IBAction func birth_date(_ sender: UITextField) {
-        birth_date_attr.text = sender.text!;
+    @IBAction func birth_date(_ sender: UIDatePicker) {
+        birth_date_attr = sender;
     }
-    @IBAction func city(_ sender: UITextField) {
-        city_attr.text = sender.text!;
+    @IBAction func city(_ sender: UIPickerView) {
+        city_picker = sender;
 
     }
-    @IBAction func gender(_ sender: UITextField) {
-        gender_attr.text = sender.text!;
+    @IBAction func gender(_ sender: UIPickerView) {
+        gender_picker = sender;
     }
     @IBAction func applied_vaccine(_ sender: UITextField) {
         applied_vaccine_attr.text = sender.text!;
@@ -68,7 +76,6 @@ class ViewController: UIViewController {
             title: "ILK iOS UYGULAMAM",
             message: name_surname_attr.text,
             preferredStyle: UIAlertController.Style.alert)
-
                alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default,
        handler: nil))
                present(alertController, animated: true, completion: nil)
