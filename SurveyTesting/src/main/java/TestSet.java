91242021faac0e85a5c49321af9f4d17e3d4060d
Emre derman <emre.derman@ug.bilkent.edu.tr>
2022-03-29 19:19:34 +0300

    tests handled

--- SurveyTesting/src/main/java/TestSet.java
+++ SurveyTesting/src/main/java/TestSet.java
@@ -13,15 +13,67 @@ import lombok.Getter;
 public class TestSet {
     private final IOSDriver driver;
 
-
     public TestSet(IOSDriver driver) {
         this.driver = driver;
     }
+
+    public void run() {
+        test();
+        test_2();
+        test_3();
+        test_4();
+        test_5();
+    }
+    // With Empty inputs
+    private boolean test_5() {
+        try{
+            AppModel model = new AppModel(driver);
+            model.fillForm("",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "");
+            return model.isSubmitButtonVisible();
+        }catch (Exception e){
+            return false;
+        }
+    }
+    // Test the extensive string parameter
+    private boolean test_4() {
+        try {
+            AppModel model = new AppModel(driver);
+            model.fillForm("adwlkadlwwlakdjawkljdklwajdklwajdlkawjdkljawdkkawdljawkldawkljdklawjdlwajdawlkdjklawjdkllawjdlwadjawlkdjlakwjdljwakwdjlkdajlkwjdlkdjwalkjdlwkadkljawlkdjwlakjdljawdjlkjdwakdawljwakljdawldawldwaj",
+                    "Derman",
+                    "",
+                    "Ankara",
+                    "Male",
+                    "Biontecth",
+                    "Pain",
+                    " None");
+            return model.isSubmitButtonVisible();
+        } catch (Exception e){
+            return false;
+        }
+    }
+
+    // Empty input case and add button relation
+    public boolean test_3() {
+        try {
+            AppModel model = new AppModel(driver);
+            model.fillForm("Emre", "Derman", "", "Ankara", "Male", "Biontecth", "Pain", " None");
+            return model.isSubmitButtonVisible();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    //fills all fields appropriate.
     public boolean test_2()
     {
         try {
             AppModel model = new AppModel(driver);
-            // Filling all required fields
             model.fillForm("Test", "Test", "01.10.2022", "Ankara", "Male", "Biontecth","Pain"," None");
             if (!model.isSubmitButtonVisible()) {
                 return false;
@@ -32,34 +84,11 @@ public class TestSet {
         }
 
     }
-    public void run() {
-        test();
-        test_2();
-    }
-
+    // Test the city field
     private boolean test() {
         try {
             AppModel model = new AppModel(driver);
-            model.fillForm("Emre", "Derman", "", "Ankara", "Male", "Biontecth","Pain"," None");
-            if (!model.isSubmitButtonVisible()) {
-                return false;
-            }
-            model.enterName("");
-            if (model.isSubmitButtonVisible()) {
-                return false;
-            }
-            model.enterName("Test");
-            model.enterSurname("");
-            if (model.isSubmitButtonVisible()) {
-                return false;
-            }
-            model.enterSurname("Test");
-            model.enterCity("");
-            if (model.isSubmitButtonVisible()) {
-                return false;
-            }
-            model.enterCity("Izmir");
-          model.toggleSideEffects("Pain and rush");
+            model.fillForm("Emre", "Derman", "", "kara", "Male", "Biontecth","Pain"," None");
             return model.isSubmitButtonVisible();
         } catch (Exception e) {
             return false;
